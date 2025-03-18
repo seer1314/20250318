fa19d8ed4e227b094437a019975ed0f39a7c6064
seer1314 <a0923552212@gmail.com>
2025-03-18 11:19:41 +0800

    v0

---
+++ web.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+from datetime import datetime
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello 王啓名!"
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html", datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.run()
